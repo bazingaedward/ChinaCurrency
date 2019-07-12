fd0b143b9569de4af4b3a239feeb9f05e941b964
Edward Qu <kaixiang.qiu@yitu-tech.com>
2019-07-12 17:45:53 +0800

    feat: 添加测试和更新readme

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,23 +1,50 @@
 const formatRMB = require('../index');
 
-// eg1: 全量位数
-// const num = 123412341234.56
-// 人民币壹仟贰佰叁拾肆亿壹仟贰佰叁拾肆万壹仟贰佰叁拾肆元伍角陆分
+test('小数-两位-整数为0', ()=>{
+  const num = 0.56;
+  let res = formatRMB(num)
+  console.log(res)
+  expect(res.value).toBe('人民币伍角陆分')
+})
 
-// const num = 123412341234.5
-// 人民币壹仟贰佰叁拾肆亿壹仟贰佰叁拾肆万壹仟贰佰叁拾肆元伍角
+test('小数-两位', ()=>{
+  const num = 1234.56;
+  let res = formatRMB(num)
+  expect(res.value).toBe('人民币壹仟贰佰叁拾肆元伍角陆分')
+})
 
-// const num = 123412341234.06
-// 人民币壹仟贰佰叁拾肆亿壹仟贰佰叁拾肆万壹仟贰佰叁拾肆元零陆分
+test('小数-角', ()=>{
+  const num = 123412341234.5;
+  let res = formatRMB(num)
+  expect(res.value).toBe('人民币壹仟贰佰叁拾肆亿壹仟贰佰叁拾肆万壹仟贰佰叁拾肆元伍角')
+})
 
+test('小数-分', ()=>{
+  const num = 123412341234.06;
+  let res = formatRMB(num)
+  expect(res.value).toBe('人民币壹仟贰佰叁拾肆亿壹仟贰佰叁拾肆万壹仟贰佰叁拾肆元零陆分')
+})
 
-// const num = 1234567890
-// 人民币壹拾贰亿叁仟肆佰伍拾陆万柒仟捌佰玖拾零元整
+test('整数', ()=>{
+  const num = 1234567890;
+  let res = formatRMB(num)
+  expect(res.value).toBe('人民币壹拾贰亿叁仟肆佰伍拾陆万柒仟捌佰玖拾元整')
+})
 
-// const num = 1002
-// 人民币壹仟零贰元整
+test('整数-间隔零', ()=>{
+  const num = 1002;
+  let res = formatRMB(num)
+  expect(res.value).toBe('人民币壹仟零贰元整')
+})
 
-const num = 1000
-// 人民币壹仟元整
+test('整数-全部零', ()=>{
+  const num = 1000;
+  let res = formatRMB(num)
+  expect(res.value).toBe('人民币壹仟元整')
+})
 
-console.log(formatRMB(num))
+test('前缀', ()=>{
+  const num = 1000;
+  let res = formatRMB(num, '$')
+  expect(res.value).toBe('$壹仟元整')
+})
